e8754b3e6f61e31c3720825379aa82c19f6bf8a4
nahidul31 <nahidulislam2383@email.com>
2026-05-25 21:03:46 +0600

    try to solve deploy problem

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const db = client.db("StudyNookDb");
     const roomCollection = db.collection("allRooms");
     // get data----------
@@ -117,7 +117,7 @@ async function run() {
 
       res.json(result);
     });
-    //delete room
+    //delete room--------------
     app.delete("/rooms/:id", async (req, res) => {
       const { id } = req.params;
 
@@ -132,7 +132,7 @@ async function run() {
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!",
+      "Pinged your deployment. You successfully connected to MongoDB",
     );
   } finally {
     // Ensures that the client will close when you finish/error
